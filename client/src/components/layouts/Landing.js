196ecad8611b87b5fc97b61f4c9575326018de1b
Prince Joey Lee <princejoeylee12@gmail.com>
2021-03-13 20:35:38 +0800

    maximize load time by adding React.Lazy()

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
+
+//material ui
+import { CircularProgress } from '@material-ui/core';
 
 //components
-import Events from '../events/Events';
+const RenderEvents = React.lazy(() => import('../events/Events'));
 
 const Landing = () => {
   return (
     <Fragment>
-      <Events />
+      <Suspense
+        fallback={
+          <div style={{ paddingTop: '50px', textAlign: 'center' }}>
+            <CircularProgress />
+          </div>
+        }>
+        <RenderEvents />
+      </Suspense>
     </Fragment>
   );
 };
